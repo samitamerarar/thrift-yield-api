ca13096737346b9852526c32a3e901b70745ea34
samitamerarar <sami.t.arar@hotmail.com>
2024-01-13 19:22:11 -0500

    Implemented user model

--- README.md
+++ README.md
@@ -12,6 +12,25 @@ Mapped through docker-compose _volumes_ **./app:/app**
 
 `docker-compose run --rm app sh -c "python manage.py startapp core"`
 
+### Make migrations
+
+add migrations
+
+`docker-compose run --rm app sh -c "python manage.py makemigrations"`
+
+apply migrations to the database
+
+`docker-compose run --rm app sh -c "python manage.py wait_for_db && python manage.py migrate"`
+
+#### Clear data in Database
+
+clear volume
+
+`docker volume ls`
+`docker-compose down`
+`docker volume rm thrift-yield-api_dev-db-data`
+`docker-compose run --rm app sh -c "python manage.py wait_for_db && python manage.py migrate"`
+
 ## Run linting
 
 `docker-compose run --rm app sh -c "flake8"`
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+"""
+Tests for models.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test123@example.com'
+        password = 'password123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'password123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'password123')
