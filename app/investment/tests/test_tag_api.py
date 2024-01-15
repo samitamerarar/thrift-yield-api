7c427cf875730de1391e2667ca25355dd0f932b3
samitamerarar <sami.t.arar@hotmail.com>
2024-01-14 19:44:47 -0500

    Added delete tag

--- app/investment/tests/test_tag_api.py
+++ app/investment/tests/test_tag_api.py
@@ -16,6 +16,11 @@ from investment.serializers import TagSerializer
 TAGS_URL = reverse('investment:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail url."""
+    return reverse('investment:tag-detail', args=[tag_id])
+
+
 def create_user(email='user@example.com', password='password123'):
     """Create and return a user."""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -66,3 +71,26 @@ class PRivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='Crypto')
+
+        payload = {'name': 'ETF'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+        tag = Tag.objects.create(user=self.user, name='Crypto')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
